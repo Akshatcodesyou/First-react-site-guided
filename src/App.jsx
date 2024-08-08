77a5bc16e8ed298938ca4dabc15174ea50692875
Akshat Singh <144692981+Akshatcodesyou@users.noreply.github.com>
2024-08-08 11:23:39 +0400

    update

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection';
+import FeatureSection from './components/FeatureSection';
+import Workflow from './components/Workflow';
+import Pricing from './components/Pricing';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div className='max-w-7xl mx-auto pt-20 px-6'>
+      <HeroSection />
+      <FeatureSection />
+      <Workflow />
+      <Pricing />
+      </div>
+    </>
+  )
+}
+
+export default App
